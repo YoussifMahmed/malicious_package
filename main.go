5faa0db3e87c97df25c8cf8a0ae8d6fc88e422c3
YoussifMahmed <150185325+YoussifMahmed@users.noreply.github.com>
2025-04-12 02:04:05 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,16 +1,7 @@
 package main
 
-import (
-    "fmt"
-    "os"
-)
+import "fmt"
 
-func init() {
-    // Payload: read the flag and print it
-    data, err := os.ReadFile("/root/flag.txt")
-    if err != nil {
-        fmt.Println("[!] Failed to read flag:", err)
-        return
-    }
-    fmt.Println("🏴 FLAG:", string(data))
+func main() {
+    fmt.Println("Hello, malicious package!")
 }
